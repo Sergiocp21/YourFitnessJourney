7a34538d2243b95cefe7305ba018cbea0576772f
Sergiocp <sergiocp012003@gmail.com>
2025-06-06 02:46:40 +0200

    Avoid errors wiht mongodb

--- Backend/backend/src/main/java/com/your_fitness_journey/backend/BackendApplication.java
+++ Backend/backend/src/main/java/com/your_fitness_journey/backend/BackendApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
+@EnableMongoRepositories(basePackages = "com.your_fitness_journey.backend.Repository.MongoDB")
 public class BackendApplication {
 
     public static void main(String[] args) {
@@ -20,7 +22,7 @@ public class BackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173", "https://yourfitnessjourney.fit:5173");
             }
         };
     }
